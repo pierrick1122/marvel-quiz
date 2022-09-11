41f96553ab85d5c860b8776a6c8223bef83e7241
Pierrick Reille <pierrick11@hotmail.fr>
2022-09-11 12:21:54 +0200

    added animation on wolwerine's claws when mouseOver and mouseOut on btns

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -14,14 +14,30 @@ const Landing = () => {
     }, 1000);
   }, [])
 
+  const setLeftImg = () => {
+    refWolverine.current.classList.add("leftImg");
+  }
+
+  const setRightImg = () => {
+    refWolverine.current.classList.add("rightImg");
+  }
+
+  const clearImg = () => {
+    if(refWolverine.current.classList.contains("leftImg")) {
+      refWolverine.current.classList.remove("leftImg")
+    } else if(refWolverine.current.classList.contains("rightImg")) {
+      refWolverine.current.classList.remove("rightImg")
+    }
+  }
+
   const displayBtn = btn && (
     <Fragment>
       <div className="leftBox">
-          <button className="btn-welcome">Inscription</button>
+          <button onMouseOver={setLeftImg} onMouseOut={clearImg} className="btn-welcome">Inscription</button>
       </div>
 
       <div className="rightBox">
-          <button className="btn-welcome">Connexion</button>
+          <button onMouseOver={setRightImg} onMouseOut={clearImg} className="btn-welcome">Connexion</button>
       </div>
     </Fragment>
   )
