cc41e09946a905da8a71d2a7c74591fbc67550cf
Pierrick Reille <pierrick11@hotmail.fr>
2022-09-11 11:43:02 +0200

    created components Header, landing and footer

---
+++ src/components/App/index.js
@@ -0,0 +1,16 @@
+import '../../App.css';
+import Header from '../Header/index';
+import Landing from '../Landing/index';
+import Footer from '../Footer/index';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Landing />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
