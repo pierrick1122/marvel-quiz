f0ef361491b29772b1adea4d0c9a67498747467b
Pierrick Reille <pierrick11@hotmail.fr>
2022-09-12 15:01:04 +0200

    added style on error page + img

--- src/components/ErrorPage/index.js
+++ src/components/ErrorPage/index.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import Batman from '../../images/batman.png';
+
+const centerH2 = {
+  textAlign: 'center',
+  marginTop: '50px'
+}
+
+const centerImg = {
+  display: 'block',
+  margin: '40px auto'
+}
 
 const ErrorPage = () => {
   return (
     <div className="quiz-bg">
       <div className="container">
-        ErrorPage
+        <h2 style={centerH2}>Oups, this page does'nt exist!</h2>
+        <img src={Batman} style={centerImg} alt="error page" />
       </div>
     </div>
   )
