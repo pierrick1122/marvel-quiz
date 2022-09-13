51ddf934c8d6bd9bd2c8ee540c0d186bf048d3d4
Pierrick Reille <pierrick11@hotmail.fr>
2022-09-13 10:42:58 +0200

    installed firebase and config it

---
+++ src/components/Firebase/firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAi77hNIeMWQ85hBWoR1zAh3BFL7OuvIkQ",
+  authDomain: "marvel-quiz-c6a34.firebaseapp.com",
+  projectId: "marvel-quiz-c6a34",
+  storageBucket: "marvel-quiz-c6a34.appspot.com",
+  messagingSenderId: "983225613737",
+  appId: "1:983225613737:web:4dcb3167f377038a0f18b8"
+};
+
+const app = initializeApp(firebaseConfig);
