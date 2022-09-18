0197fb6caa0569fb211863c216f4458efbd93be4
Pierrick Reille <pierrick11@hotmail.fr>
2022-09-18 11:07:01 +0200

    added that its possible to go on /welcome only if connected

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState, Fragment, useEffect } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
 import Logout from '../Logout/index';
 import Quiz from '../Quiz/index';
+import { auth } from '../Firebase/firebaseConfig';
+import { useNavigate } from 'react-router-dom';
 
 const Welcome = () => {
-  return (
+
+  const navigate = useNavigate();
+  const [userSession, setUserSession] = useState(null);
+
+  useEffect(() => {
+    const listener = onAuthStateChanged(auth, (user) => {
+      user ? setUserSession(user) : navigate('/')
+    })
+    return listener();
+  }, [])
+
+  return userSession === null ? (
+    <Fragment>
+      <div className="loader"></div>
+      <p className="loaderText">Loading...</p>
+    </Fragment>
+  ) : (
     <div className="quiz-bg">
       <div className="container">
         <Logout />
